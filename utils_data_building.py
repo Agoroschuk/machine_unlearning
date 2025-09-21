677f43c2d7be16a85a30525b97f732a55ae35239
Agoroschuk <sukhanovaas01@gmail.com>
2025-09-21 18:04:16 +0000

    study v1

--- utils_data_building.py
+++ utils_data_building.py
@@ -37,51 +37,59 @@ class Rule:
         
     def get_up_edges_list(self, edge_list, edge_type_list, unlearn_edge, unlearn_edge_type):
         """
-        Восходящий поиск
+        По выводу ищет его причины
+        Вроде мы ищем случаи, похожие на рассматриваемое правило. И по их выводам ищем, что к ним привело
 
         Если (X отец Y) и (Y отец Z), то (X дед Z)
         Мы знаем: (0 дед 2) - хотим найти КАК это получилось
-        edge_list - существующие связи: [(0,1), (1,2), (3,4)]
-        edge_type_list - типы связей: ['father', 'brother', 'husband']
-        unlearn_edge - конкретная связь которую объясняем: (0, 2) 
+        edge_list - существующие связи: [(10, 20), (20, 30), (20, 40)]  # (source, target)
+        edge_type_list - типы связей: ['father', 'brother', 'brother']  
+        unlearn_edge - конкретная связь которую объясняем: (10, 30) 
         unlearn_edge_type - тип этой связи: 'uncle'
         """
-        # пример ниже расписан для данных из головы, не данных из docstring выше
+        # пример: (10, 20) father
         source_type_dict = {}
         type_target_dict = {}
         for edge, edge_type in zip(edge_list, edge_type_list):
-            source_type = (edge[0], edge_type) #(0, father)
+            source_type = (edge[0], edge_type) #(10, father)
             if source_type in source_type_dict.keys():
-                source_type_dict[source_type].append(edge[1]) #(0, father): 2 '2 = отец для 0'
-                # но тогда у 0 есть 2 отца ))))))
+                source_type_dict[source_type].append(edge[1]) # не заполнится для этого примера
             else:
-                source_type_dict[source_type] = [edge[1]] # (0, father): 1 '1 = отец для 0' 
+                source_type_dict[source_type] = [edge[1]] # (10, father): 20 '20 = отец для 10' 
                 
-            type_target = (edge_type, edge[1]) # (father, 1)
+            type_target = (edge_type, edge[1]) # (father, 20)
             if type_target in type_target_dict.keys():
-                type_target_dict[type_target].append(edge[0]) # (father, 1): 7 '1 = отец для 7'
+                type_target_dict[type_target].append(edge[0]) # не заполнится для этого примера, но ключ м.б. не один
             else:
-                type_target_dict[type_target] = [edge[0]] # (father, 1): 0  '1 = отец для 0'
+                type_target_dict[type_target] = [edge[0]] # (father, 20): 10 '20 = отец для 10'
         
         var_value = -np.ones(self.num_var) # делает массив из -1 размером num_var
-        var_value[self.right_tuple[0]] = unlearn_edge[0] # 0
-        var_value[self.right_tuple[2]] = unlearn_edge[1] # 2
+        var_value[self.right_tuple[0]] = unlearn_edge[0] # 10
+        var_value[self.right_tuple[2]] = unlearn_edge[1] # 30
+        # var_value = [10, -1, 30] для рассматриваемого примера
         
         dc_var_value_list = []
         
-        # Вроде как ищем все, что приводит к забываемому факту здесь
+        # Вроде как ищем все, что приводит к забываемому факту здесь с помощью рекурсивного поиска
         def _get_up_edges_list(cur_var_value):
-            if (cur_var_value == -1).sum() == 0:
+            # не зайдем сюда, пока -1 из [0, -1, 2] не заменится значением != -1
+            if (cur_var_value == -1).sum() == 0: # зашли при cur_val_value = [10, 20, 30]
+                # for tup in (0, 'father', 1), (1, 'brother', 2), (0, 'uncle', 2)
                 for tup in self.left_tuples + [self.right_tuple]:
+                    # если нет в ключах
                     if (cur_var_value[tup[0]], tup[1]) not in source_type_dict.keys():
-                        return
+                        return # пустой return прерывает текущую ветку поиска, если рассматриваемое отношение не вписывается в rule
+                    # если нет в значениях
                     if cur_var_value[tup[2]] not in source_type_dict[(cur_var_value[tup[0]], tup[1])]:
                         return
-                
+                # чтобы в dc_var_value_list были уникальные решения
                 if not any(np.array_equal(cur_var_value, unique_arr) for unique_arr in dc_var_value_list):
+                    # кладем, например, [10, 20, 30]
                     dc_var_value_list.append(cur_var_value)
                 return
             
+            # left_tuples = (0, 'father', 1), (1, 'brother', 2)
+            # cur_val_value = [10, -1, 30]
             for tup in self.left_tuples:
                 if cur_var_value[tup[0]] == -1 and cur_var_value[tup[2]] != -1:
                     if (tup[1], cur_var_value[tup[2]]) in type_target_dict.keys():
@@ -89,27 +97,38 @@ class Rule:
                             new_cur_var_value = deepcopy(cur_var_value)
                             new_cur_var_value[tup[0]] = potential_tup0_val
                             _get_up_edges_list(new_cur_var_value)
+                # заходим сюда для (0, 'father', 1)
+                # cur_var_value[1] == -1 and cur_var_value[0] != -1:
                 elif cur_var_value[tup[2]] == -1 and cur_var_value[tup[0]] != -1:
+                    # if (10, father) in source_type_dict.keys()
                     if (cur_var_value[tup[0]], tup[1]) in source_type_dict.keys():
+                        # смотрим значения для ключа (10, father), например, 20
                         for potential_tup0_val in source_type_dict[(cur_var_value[tup[0]], tup[1])]:
                             new_cur_var_value = deepcopy(cur_var_value)
+                            # заменяем неизвестное значение -1 отношением 20
+                            # получается new_cur_var_value = [10, 20, 30]
                             new_cur_var_value[tup[2]] = potential_tup0_val
                             _get_up_edges_list(new_cur_var_value)
         
         _get_up_edges_list(var_value)
         
         up_edges_list = []
+        # пусть dc_var_value= [10, 20, 30]
         for dc_var_value in dc_var_value_list:
             up_edges = []
-            for tup in self.left_tuples:
+            # left_tuples = (0, 'father', 1), (1, 'brother', 2)
+            for tup in self.left_tuples: 
+                # up_edges.append(10, father, 20)
+                # up_edges.append(20, brother, 30) --> получили цепочку для 1-го решения [10, 20, 30]
                 up_edges.append((dc_var_value[tup[0]], tup[1], dc_var_value[tup[2]]))
             up_edges_list.append(up_edges)
-            
+        
+        # итак, в up_edges_list - цепочка фактов, которая привела к выводу, получено на основании введенных логических правил
         return up_edges_list
     
     def get_dc_edges_list(self, edge_list, edge_type_list, person_list):
         """
-        Нисходящий поиск
+        Ищет все возможные связи, которые могут быть выведены из существующих с помощью правил
         """
         source_type_dict = {}
         type_target_dict = {}
