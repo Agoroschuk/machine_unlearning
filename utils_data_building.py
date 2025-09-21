ba96963b1cc05978afebf4344f55976ea6f0303b
Agoroschuk <sukhanovaas01@gmail.com>
2025-09-21 19:31:44 +0000

    study v2

--- utils_data_building.py
+++ utils_data_building.py
@@ -28,8 +28,7 @@ class Rule:
         all_tuples = left_tuples + [right_tuple]
         [(0, 'father', 1), (1, 'brother', 2), (0, 'uncle', 2)]
         num_var = 3 
-        Я так понимаю, что left_tuples и right_tuple должны уже характеризовать 
-        корректное правило
+        Left_tuples и right_tuple характеризуют корректное правило
         """
         self.left_tuples = left_tuples
         self.right_tuple = right_tuple
@@ -37,11 +36,10 @@ class Rule:
         
     def get_up_edges_list(self, edge_list, edge_type_list, unlearn_edge, unlearn_edge_type):
         """
-        По выводу ищет его причины
-        Вроде мы ищем случаи, похожие на рассматриваемое правило. И по их выводам ищем, что к ним привело
+        По выводу ищет его причины, что к нему привело, опираясь на связи в правилах.
 
         Если (X отец Y) и (Y отец Z), то (X дед Z)
-        Мы знаем: (0 дед 2) - хотим найти КАК это получилось
+        Мы знаем: (0 дед 2) - хотим найти какой цепочкой взаимосвязей это получилось
         edge_list - существующие связи: [(10, 20), (20, 30), (20, 40)]  # (source, target)
         edge_type_list - типы связей: ['father', 'brother', 'brother']  
         unlearn_edge - конкретная связь которую объясняем: (10, 30) 
@@ -89,7 +87,7 @@ class Rule:
                 return
             
             # left_tuples = (0, 'father', 1), (1, 'brother', 2)
-            # cur_val_value = [10, -1, 30]
+            # cur_val_value = [10, -1, 30]  (промежуточный факт неизвестен)
             for tup in self.left_tuples:
                 if cur_var_value[tup[0]] == -1 and cur_var_value[tup[2]] != -1:
                     if (tup[1], cur_var_value[tup[2]]) in type_target_dict.keys():
@@ -124,14 +122,24 @@ class Rule:
             up_edges_list.append(up_edges)
         
         # итак, в up_edges_list - цепочка фактов, которая привела к выводу, получено на основании введенных логических правил
-        return up_edges_list
+        return up_edges_list # [(10, father, 20), (20, brother, 30)] получили на основании unlearn_edge (10, 30), unlearn_edge_type = 'uncle'
     
     def get_dc_edges_list(self, edge_list, edge_type_list, person_list):
         """
-        Ищет все возможные связи, которые могут быть выведены из существующих с помощью правил
+        Ищет все возможные связи, которые могут быть выведены из существующих с помощью правил.
+        Связи должны удовлетворять требованиям по полу и не сущестствовать еще в графе.
+
+        Рассмотрим тот же пример:
+        left_tuples = [(0, 'father', 1), (1, 'brother', 2)]
+        right_tuple = (0, 'uncle', 2)
+
+        edge_list = [(10, 20), (20, 30)] 
+        edge_type_list = ['father', 'brother'] 
         """
-        source_type_dict = {}
-        type_target_dict = {}
+    
+        source_type_dict = {} # (10, 'father'): [20], (20, 'brother'): [30] 
+        type_target_dict = {} # ('father', 20): [10], ('brother', 30): [20]
+
         for edge, edge_type in zip(edge_list, edge_type_list):
             source_type = (edge[0], edge_type)
             if source_type in source_type_dict.keys():
@@ -147,16 +155,24 @@ class Rule:
         
         dc_var_value_list = []
         def _get_right_edges_list(cur_var_value):
+            # cur_var_value = [10, 20, 30]
             if (cur_var_value == -1).sum() == 0:
                 for tup in self.left_tuples:
+                    # есть ли такой ключ?
                     if (cur_var_value[tup[0]], tup[1]) not in source_type_dict.keys():
                         return
+                    # есть ли такое значение?
                     if cur_var_value[tup[2]] not in source_type_dict[(cur_var_value[tup[0]], tup[1])]:
                         return
+                # чтобы решения не дублировались
                 if not any(np.array_equal(cur_var_value, unique_arr) for unique_arr in dc_var_value_list):
+                    # dc_var_value_list = [10, 20, 30]
                     dc_var_value_list.append(cur_var_value)
                 return
             
+            # self.left_tuples = (0, 'father', 1) --> не подходит под условия, переходим к след. tup
+            # ---> (1, 'brother', 2)
+            # cur_val_value = [10, 20, -1]
             for tup in self.left_tuples:
                 if cur_var_value[tup[0]] == -1 and cur_var_value[tup[2]] != -1:
                     if (tup[1], cur_var_value[tup[2]]) in type_target_dict.keys():
@@ -164,47 +180,66 @@ class Rule:
                             new_cur_var_value = deepcopy(cur_var_value)
                             new_cur_var_value[tup[0]] = potential_tup0_val
                             _get_right_edges_list(new_cur_var_value)
+                # elif cur_val_value[2] == -1 and cur_val_value[1] = 10:
                 elif cur_var_value[tup[2]] == -1 and cur_var_value[tup[0]] != -1:
+                    # if (20, brother) in ((10, father), (20, brother)):
                     if (cur_var_value[tup[0]], tup[1]) in source_type_dict.keys():
+                        # см. возможные ключи для (20, brother): 30
                         for potential_tup0_val in source_type_dict[(cur_var_value[tup[0]], tup[1])]:
                             new_cur_var_value = deepcopy(cur_var_value)
+                            # new_cur_var_value = [10, 20, 30]
                             new_cur_var_value[tup[2]] = potential_tup0_val
                             _get_right_edges_list(new_cur_var_value)
         
+        # for (10, 20), father
         for edge, edge_type in zip(edge_list, edge_type_list):
+            # if father == father
             if edge_type == self.left_tuples[0][1]:
+                # var_value = [-1, -1, -1]
                 var_value = -np.ones(self.num_var)
                 var_value[self.left_tuples[0][0]] = edge[0]
                 var_value[self.left_tuples[0][2]] = edge[1]
+                # итог: var_value = [10, 20, -1]  (cледствие неизвестно)
                 _get_right_edges_list(var_value)
+                # после этого получили в dc_var_value_list все возможные тройки вершин на основании данных
         
         
         new_edge_list = []
         new_edge_type_list = []
         
+        # dc_var_value = [10, 20, 30]
+        # self.right_tuple = (0, 'uncle', 2)
         for dc_var_value in dc_var_value_list:
+            # new_edge = (10, 30)
             new_edge = (dc_var_value[self.right_tuple[0]], dc_var_value[self.right_tuple[2]])
+            # new_edge_type = 'uncle'
             new_edge_type = (self.right_tuple[1])
             
+            # if (10, 'uncle') in ((10, 'father'), (20, 'brother')):
             if (dc_var_value[self.right_tuple[0]], self.right_tuple[1]) in source_type_dict.keys():
                 if dc_var_value[self.right_tuple[2]] in source_type_dict[(dc_var_value[self.right_tuple[0]], self.right_tuple[1])]:
                     continue
-            
+            # if 'uncle': in ...
             if self.right_tuple[1] in ["husband", "uncle", "father", "brother", "nephew"]:
+                # проверка пола, если родственная связь мужская
+                # person_list из класса Person видимо
                 if person_list[int(dc_var_value[self.right_tuple[2]])].gender != "male":
                     continue
+                # исключаем однополые браки :)
                 if self.right_tuple[1] == "husband" and person_list[int(dc_var_value[self.right_tuple[0]])].gender != "female":
                     continue
-                    
+            # симметрично относительно женского пола      
             elif self.right_tuple[1] in ["wife", "aunt", "mother", "sister", "niece"]:
                 if person_list[int(dc_var_value[self.right_tuple[2]])].gender != "female":
                     continue
                 if self.right_tuple[1] == "wife" and person_list[int(dc_var_value[self.right_tuple[0]])].gender != "male":
                     continue
+            # if dc_var_value[0] == dc_var_value[2] (типа если сам себе отец, пропускаем)
             if dc_var_value[self.right_tuple[0]] == dc_var_value[self.right_tuple[2]]:
                 continue
             
+            # new_edge_list = [10, 30], вывели из (10, 20) и (20, 30)
             new_edge_list.append(new_edge)
-            new_edge_type_list.append(new_edge_type)
+            new_edge_type_list.append(new_edge_type) # uncle
             
-        return new_edge_list, new_edge_type_list
+        return new_edge_list, new_edge_type_list # (10, 30), uncle
