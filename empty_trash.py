0fb0893facc308bf941440d81f35cf391237fe7b
Anastasia Goroschuk <sukhanovaas01@gmail.com>
2026-04-13 16:33:58 +0300

    gdrive trash script upd

--- empty_trash.py
+++ empty_trash.py
@@ -8,6 +8,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 # Если вы меняете этот scope, нужно будет удалить файл token.pickle
+PATH = '/content/drive/MyDrive/miscellaneous/vscode_connection/gdrive_cleaning/'
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def get_authenticated_service():
@@ -15,10 +16,12 @@ def get_authenticated_service():
     Аутентификация пользователя и создание сервиса для работы с Google Drive API.
     Токен сохраняется в файл token.pickle для последующих запусков.
     """
-    creds = None
+
+    # creds - объект класса google.oauth2.credentials.Credentials, сначала учетных данных нет
+    creds = None # bool(None) = False
     # Файл token.pickle хранит токены доступа пользователя
     if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+        with open(f'{PATH}/token.pickle', 'rb') as token:
             creds = pickle.load(token)
 
     # Если нет валидных учетных данных, заставляем пользователя залогиниться
@@ -29,13 +32,13 @@ def get_authenticated_service():
             # ВАЖНО: Замените 'credentials.json' на путь к вашему файлу с учетными данными OAuth 2.0
             # Скачайте его из Google Cloud Console.
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                f'{PATH}/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Сохраняем токен для следующего запуска
-        with open('token.pickle', 'wb') as token:
+        with open(f'{PATH}/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    # Возвращаем сервис для работы с API v3
+    # Возвращаем сервис (клиентский объект) для работы с Google Drive API v3
     service = build('drive', 'v3', credentials=creds)
     return service
 
@@ -48,7 +51,7 @@ def empty_trash():
     
     print("Очищаю корзину Google Диска...")
     try:
-        # Вызов метода emptyTrash. 
+        # Вызов метода emptyTrash = полная очистка корзины
         # Согласно документации, при успешном выполнении возвращается пустой ответ (None) [citation:2][citation:3].
         service.files().emptyTrash().execute()
         print("✅ Корзина Google Диска успешно очищена.")
